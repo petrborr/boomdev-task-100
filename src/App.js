08b0e4892b4bbffefb85e22b8139b582685558c0
petrborr <petrborr@gmail.com>
2022-12-28 17:19:40 +0200

    fix: switch component styling from normal css to inline css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
 
+const styles = {
+wrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "1rem",
+    paddingBottom: "1rem",
+    borderBottom: "1px solid #ccc"
+  },
+  mealHeading: {
+    margin: "0 0 0.25rem 0"
+  },
+  description: {
+    fontStyle: "italic"
+  },
+  price: {
+    marginTop: "0.25rem",
+    fontWeight: "bold",
+    color: "#ad5502",
+    fontSize: "1.25rem"
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
-        <div className="description">I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+    <div>
+      <div style={styles.wrapper}>
+        <h3 style={styles.mealHeading}>Sushi</h3>
+        <div style={styles.description}>I prefer steak than sushi.</div>
+        <div style={styles.price}>$9.99</div>
       </div>
     </div>
   );
